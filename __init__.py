4a1a468f241564cc68339ca6afcfef9961324fe1
walter <xiao.feng@yourdream.cc>
2024-07-18 20:17:01 +0800

    feat: add color checker node

--- __init__.py
+++ __init__.py
@@ -1,7 +1,9 @@
 from .nodes.brightness import CalculateImageBrightness
 from .nodes.load_image_by_url import LoadImageByUrlOrPath
+from .nodes.color_similarity_checker import ColorSimilarityChecker
 
 NODE_CLASS_MAPPINGS = {
     "Calculate Image Brightness": CalculateImageBrightness,
     "Load Image (By Url)": LoadImageByUrlOrPath,
+    "Color Similarity Checker": ColorSimilarityChecker,
 }
